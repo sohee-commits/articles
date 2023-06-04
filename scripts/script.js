114821d1f39cfae2e3b30d135b9a74d7c9a2301b
mintopiate <105264974+mintopiate@users.noreply.github.com>
2023-06-04 19:07:25 +0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        {pageLanguage: "en" ,
+        includedLanguages:'en,ru,ko,ja',},
+        "google_translate_element"
+    );
+}
